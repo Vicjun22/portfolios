57b5ddf4d0e7e5cd95784f5643cb072edbca6e05
Victor Ross <vicjun22@gmail.com>
2023-05-03 23:17:57 -0300

    Update commit

--- portfolio-karol/src/components/inicio/inicio.component.tsx
+++ portfolio-karol/src/components/inicio/inicio.component.tsx
@@ -1,19 +1,22 @@
 import './inicio.style.css';
 
-import KarolImg from '../../assets/images/Karol-Hi.svg'
 import { useEffect, useState } from 'react';
 
+import KarolImg from '../../assets/images/Karol-Hi.svg'
+
 export function Inicio() {
-    const [cargos, _] = useState<string[]>(['Designer', 'Designer Gráfica']);
+    const cargos: string[] = ['Designer', 'Designer Gráfica'];
     const [index, setIndex] = useState<number>(0);
+    const [view, setView] = useState<string>(cargos[index])
 
     useEffect(() => {
         const lastIndex = cargos.length - 1;
         const intervalId = setInterval(() => {
             setIndex(index => index < lastIndex ? index + 1 : 0);
-        }, 5000);
+            setView(cargos[index]);
+        }, 3000);
         return () => clearInterval(intervalId);
-    }, [cargos.length])
+    }, [index])
  
     return (
         <section className={'inicio'}>
@@ -23,7 +26,7 @@ export function Inicio() {
                     <img src={KarolImg} alt={'Ícone'} />
                 </h1>
                 <h1 className={'inicio_hello'}>
-                    e sou <span className={'inicio_hello-destaque inicio_cursor'}>{cargos[index]}</span>
+                    e sou <span className={'inicio_hello-destaque inicio_cursor'}>{view}</span>
                 </h1>
             </div>
         </section>
