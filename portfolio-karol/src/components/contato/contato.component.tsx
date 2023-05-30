3001b49f961effa337203d6a9440af6e423baf96
Victor Ross <vicjun22@gmail.com>
2023-05-29 23:00:44 -0300

    informações

--- portfolio-karol/src/components/contato/contato.component.tsx
+++ portfolio-karol/src/components/contato/contato.component.tsx
@@ -3,7 +3,9 @@ import './contato.style.css';
 export function Contato({componentID}: any) {
     return(
         <section id={componentID} className={'contato'}>
-            contato
+            Contato
+            Telefone: (51) 98457-0480
+            E-mail: karolinelemesvaz@gmail.com
         </section>
 
     )
